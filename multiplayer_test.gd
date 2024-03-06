44074d7340a33248564f8d758af4d21bd13ef6dc
HackrDonut <luke.a.pruitt@gmail.com>
2024-03-06 11:08:29 -0600

    ghost gottem
    we got a ghost workin

--- multiplayer_test.gd
+++ multiplayer_test.gd
@@ -2,12 +2,13 @@ extends Node2D
 
 var peer = ENetMultiplayerPeer.new()
 @export var player_scene: PackedScene
+@export var ghost_scene: PackedScene
 # @export var ip = IP.resolve_hostname(str(OS.get_environment("COMPUTERNAME")),1)
 
 func _on_host_pressed():
 	peer.create_server(135)
 	multiplayer.multiplayer_peer = peer
-	multiplayer.peer_connected.connect(_add_player)
+	multiplayer.peer_connected.connect(_add_ghost)
 	_add_player()
 	# print(ip)
 
@@ -15,6 +16,11 @@ func _add_player(id = 1):
 	var player = player_scene.instantiate()
 	player.name = str(id)
 	call_deferred("add_child",player)
+	
+func _add_ghost(id = 1):
+	var ghost = ghost_scene.instantiate()
+	ghost.name = str(id)
+	call_deferred("add_child",ghost)
 
 func _on_join_pressed():
 	peer.create_client("172.29.19.153", 135)
