e5dc0c6e4acd0aceaeaef5001884d82adf4c8747
HackrDonut <luke.a.pruitt@gmail.com>
2024-03-14 10:59:34 -0500

    women?
    women?

--- multiplayer_test.gd
+++ multiplayer_test.gd
@@ -5,11 +5,9 @@ var peer = ENetMultiplayerPeer.new()
 @export var ghost_scene: PackedScene
 # @export var ip = IP.resolve_hostname(str(OS.get_environment("COMPUTERNAME")),1)
 
-func _physics_process(delta):
-	pass
-	# print(player_scene.name)
 
 func _on_host_pressed():
+	print("Are we in it?")
 	peer.create_server(135)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(_add_ghost)
@@ -19,6 +17,7 @@ func _on_host_pressed():
 func _add_player(id = 1):
 	var player = player_scene.instantiate()
 	player.name = str(id)
+	print("we in it now bois")
 	call_deferred("add_child",player)
 	
 func _add_ghost(id = 1):
@@ -41,11 +40,6 @@ func _on_join_area_mouse_entered():
 
 func _on_join_area_mouse_exited():
 	$button_sprite_2.play("join_idle")
-
-func _on_door_area_body_entered(body):
-	if Input.is_action_pressed("ui_right"):
-		if body.name == "1":
-			body.position.x = 220
 			
 			# print("GO WEST!!!")
 	# if body.name == "1": # Human, not ghost
